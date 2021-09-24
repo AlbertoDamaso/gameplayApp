5f97540c49fbe278e44462f07dc891b3be53310b
AlbertoDamaso <alberto.matheus@hotmail.com>
2021-09-24 18:12:00 -0300

    main

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -39,6 +39,7 @@ function AuthProvider({ children }: AuthProviderProps){
         try {
             setLoading(true);
             
+            
             const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}
             &redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
 
